bd5b86f1a607dbc2ef8c1e92b8b30fa8abc776a4
Aztal-Samoht <thatguysquig@gmail.com>
2022-04-19 15:05:50 -0500

    fixed the initial mi_card app

---
+++ project_mi_card/android/app/src/main/kotlin/com/example/project_mi_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_mi_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- project_mi_card/lib/main.dart
+++ project_mi_card/lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: MainPage(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Demo'),
+      ),
+      body: Center(
+        child: Text('My name is not Angela.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
