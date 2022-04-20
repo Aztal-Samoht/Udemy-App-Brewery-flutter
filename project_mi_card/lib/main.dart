7fe9fd5c23a3642f55bf87bbc952fb88e721cf09
Aztal-Samoht <thatguysquig@gmail.com>
2022-04-20 16:15:31 -0500

    really, actually compleated lesson 44

--- project_mi_card/lib/main.dart
+++ project_mi_card/lib/main.dart
@@ -9,71 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //verticalDirection: VerticalDirection.up, // up, down...
-            mainAxisAlignment:
-                MainAxisAlignment.start, //end, start, spaceBetween...
-            crossAxisAlignment: CrossAxisAlignment.stretch, //start, end, center
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                // margin: EdgeInsets.all(20.0),
-                // margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-                // padding: EdgeInsets.only(top: 30),
-                color: Colors.white,
-                child: Text('Container 1'),
+            children: const [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/569Garbodor.png'),
+                radius: 50.0,
               ),
-              SizedBox(height: 20),
-              Container(
-                height: 100.0,
-                width: 300.0,
-                //margin: EdgeInsets.all(20.0),
-                // margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-                // padding: EdgeInsets.only(top: 30),
-                color: Colors.blue,
-                child: Text('Container 2'),
+              Text(
+                "squigy",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                //margin: EdgeInsets.all(20.0),
-                // margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-                // padding: EdgeInsets.only(top: 30),
-                color: Colors.yellow,
-                child: Text('Container 3'),
-              )
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Demo'),
-      ),
-      body: Center(
-        child: Text('My name is not Angela.'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        onPressed: () {},
-        child: Icon(Icons.add),
       ),
     );
   }
