e442174404dd247dda3b4fc8b5cf12070420a7b3
Aztal-Samoht <thatguysquig@gmail.com>
2022-04-22 12:03:32 -0500

    completed lesson 77, got 7 buttons working with a good layout

--- project_d_xylophone/lib/main.dart
+++ project_d_xylophone/lib/main.dart
@@ -5,18 +5,82 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('click me 1')),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    child: TextButton(onPressed: () {
+                      player.play('note1.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.orange,
+                    child: TextButton(onPressed: () {
+                      player.play('note2.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.yellow,
+                    child: TextButton(onPressed: () {
+                      player.play('note3.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.green,
+                    child: TextButton(onPressed: () {
+                      player.play('note4.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.blue,
+                    child: TextButton(onPressed: () {
+                      player.play('note5.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.indigo,
+                    child: TextButton(onPressed: () {
+                      player.play('note6.wav');
+                    }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.purple,
+                    child: TextButton(onPressed: () {
+                      player.play('note7.wav');
+                    }),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
