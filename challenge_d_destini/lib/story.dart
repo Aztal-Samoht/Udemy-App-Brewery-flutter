bccbbda5deb858e2fa71a1ebc58440a125613f93
Aztal-Samoht <thatguysquig@gmail.com>
2022-04-27 17:06:10 -0500

    completed step 8

--- challenge_d_destini/lib/story.dart
+++ challenge_d_destini/lib/story.dart
@@ -1,11 +1,35 @@
 class Story {
-  String storyTitle;
-  String choice1;
-  String choice2;
+  String _storyTitle;
+  String _choice1;
+  String _choice2;
 
   Story(String t, String c1, String c2) {
-    this.storyTitle = t;
-    this.choice1 = c1;
-    this.choice2 = c2;
+    this._storyTitle = t;
+    this._choice1 = c1;
+    this._choice2 = c2;
+  }
+
+  String getStoryTitle() {
+    return this._storyTitle;
+  }
+
+  String getChoice1() {
+    return this._choice1;
+  }
+
+  String getChoice2() {
+    return this._choice2;
+  }
+
+  void setTitle(String s) {
+    this._storyTitle = s;
+  }
+
+  void setChoice1(String s) {
+    this._choice1 = s;
+  }
+
+  void setChoice2(String s) {
+    this._choice2 = s;
   }
 }
