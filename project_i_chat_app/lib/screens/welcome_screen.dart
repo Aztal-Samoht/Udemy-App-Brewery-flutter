aafa2956ae47f607ce94ffb6a89ba9fc87a8ab61
Aztal-Samoht <thatguysquig@gmail.com>
2022-05-10 18:02:22 -0400

    extracted the NavButton widget

--- project_i_chat_app/lib/screens/welcome_screen.dart
+++ project_i_chat_app/lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_i_chat_app/screens/login_screen.dart';
 import 'package:project_i_chat_app/screens/registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:project_i_chat_app/widgets/nav_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = '/';
@@ -77,51 +79,26 @@ class _WelcomeScreenState extends State<WelcomeScreen>
             SizedBox(
               height: 48.0,
             ),
-            Text(
-              'Chat app',
-              textAlign: TextAlign.center,
+            DefaultTextStyle(
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 45.0,
                 fontWeight: FontWeight.w900,
               ),
+              child: Center(
+                child: AnimatedTextKit(
+                  animatedTexts: [TypewriterAnimatedText('Chat app')],
+                ),
+              ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
+            NavButton(
                 color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
+                text: 'Log In',
+                destination: LoginScreen.id),
+            NavButton(
                 color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+                text: 'Register',
+                destination: RegistrationScreen.id),
           ],
         ),
       ),
